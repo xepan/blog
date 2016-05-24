2c83d3a7f8f764d3b4ff0e7545e6d301dacc8da0
dkanojia <dkanojia93@gmail.com>
2016-05-24 16:59:05 +0530

    ACL added in blog post

---
+++ page/blogpost.php
@@ -0,0 +1,23 @@
+<?php
+ 
+namespace xepan\blog;
+
+class page_blogpost extends \xepan\base\Page {
+	public $title='Blog Posts';
+
+	function init(){
+		parent::init();
+
+		$blog_model = $this->add('xepan\blog\Model_BlogPost');
+		$blog_model->add('xepan\commerce\Controller_SideBarStatusFilter');
+
+		$crud = $this->add('xepan\hr\CRUD',null,null,['view/post/grid']
+						);
+
+		$crud->setModel($blog_model)->setOrder('created_at','desc');
+		$crud->grid->addPaginator(50);
+
+
+	}
+}
+
