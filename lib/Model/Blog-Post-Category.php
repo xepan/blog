51bbfbeb603e336f48b9243eba72a976325410cd
vaibhav <vaibhavsharma38@gmail.com>
2016-05-24 12:24:56 +0530

    association model made

--- lib/Model/Blog-Post-Category.php
+++ lib/Model/Blog-Post-Category.php
@@ -5,5 +5,8 @@ namespace xepan\blog;
 class Model_Blog-Post-Category extends xepan\base\Model_Table{
 	function init(){
 		parent::init();
+
+		$this->addField('name');
+		$this->hasMany('xepan\blog\Associaton_Post-Category');
 	}
 }
