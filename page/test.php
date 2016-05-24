ad81040c5e19c28376046c7f26cb735f9f4d8f77
dkanojia <dkanojia93@gmail.com>
2016-05-24 13:37:29 +0530

    extra page deleted

--- page/test.php
+++ page/test.php
@@ -5,6 +5,13 @@ class page_test extends \xepan\base\Page{
 	function init(){
 		parent::init();
 
-		$this->add('CRUD')->setModel('xepan\blog\Association_PostCategory');
+		
+		$blog_model = $this->add('xepan\blog\Model_BlogPost');
+		// $blog_model = $this->add('xepan\blog\Model_BlogPostCategory');
+		// $blog_model = $this->add('xepan\blog\Model_Comment');
+		// $blog_model = $this->add('xepan\blog\Model_Association_PostCategory');
+
+		$crud = $this->add('CRUD'
+						)->setModel($blog_model);
 	}
 }
