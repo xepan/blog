de63b2740298a11ce689894d4ba27893de1b6948
vaibhav <vaibhavsharma38@gmail.com>
2016-05-24 12:59:06 +0530

    fields added in category model

--- lib/Model/BlogPostCategory.php
+++ lib/Model/BlogPostCategory.php
@@ -3,11 +3,18 @@
 namespace xepan\blog;
 
 class Model_BlogPostCategory extends \xepan\base\Model_Table{
-	public $table='blog_post_category';
+	public $table = 'blog_post_category';
+	public $acl = false; 
 	function init(){
 		parent::init();
 
 		$this->addField('name');
+		$this->addField('type');
+		$this->addField('status');
+		$this->addCondition('type','PostCategory');
+		$this->addCondition('status','-');
+
 		$this->hasMany('xepan\blog\Associaton_PostCategory');
+		
 	}
 }
