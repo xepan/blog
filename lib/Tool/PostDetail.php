76f503e897c488f894cbad008ff8c112c1e8c538
dkanojia <dkanojia93@gmail.com>
2016-05-25 11:23:27 +0530

    detail options added

--- lib/Tool/PostDetail.php
+++ lib/Tool/PostDetail.php
@@ -3,7 +3,14 @@
 namespace xepan\blog;
 
 class Tool_PostDetail extends \xepan\cms\View_Tool{
-	public $options = [];
+	public $options = [
+						'show_description'=>true,
+						'show_title'=>true,
+						'show_image'=>true,
+						'show_comment'=>true,
+						'show_anonymous_comment'=>true
+
+						];
 	public $post;
 	function init(){
 		parent::init();
@@ -29,5 +36,42 @@ class Tool_PostDetail extends \xepan\cms\View_Tool{
 
 	function defaultTemplate(){
 		return ['view/tool/post/detail'];
+	}
+
+	function addToolCondition_row_show_title($value, $l){
+		
+		if(!$value){
+			$l->current_row_html['title_wrapper'] = "";
+			return;
+		}
+
+		$l->current_row_html['title'] =$l->model['title'];
+	}
+
+	function addToolCondition_row_show_description($value, $l){
+		
+		if(!$value){
+			$l->current_row_html['description_wrapper'] = "";
+			return;
+		}
+
+		$l->current_row_html['description'] =$l->model['description'];
+	}
+
+	function addToolCondition_row_show_tag($value, $l){
+		
+		if(!$value){
+			$l->current_row_html['tag_wrapper'] = "";
+			return;
+		}
+
+		$l->current_row_html['tag'] =$l->model['tag'];
+	}
+
+	function addToolCondition_row_show_image($value, $l){
+		if(!$value){
+			$l->current_row_html['image_wrapper'] = "";
+			return;	
+		}
 	}
 }
