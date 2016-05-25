21a26f357fe3fa1b2bfb08af6f053587fffc43b0
dkanojia <dkanojia93@gmail.com>
2016-05-25 15:50:41 +0530

    post detail tool modified

--- lib/Model/Comment.php
+++ lib/Model/Comment.php
@@ -24,6 +24,8 @@ class Model_Comment extends \xepan\base\Model_Table{
 		$this->addCondition('type','BlogComment');
 
 		$this->getElement('status')->defaultValue('Pending');
+
+		$this->addExpression('');
 	}
 
 	//Approve Post Comment
