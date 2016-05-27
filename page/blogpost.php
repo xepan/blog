28284c73f0e934fefa5efc30dbac71a9073a8064
vaibhav <vaibhavsharma38@gmail.com>
2016-05-27 09:26:10 +0530

    category lister added to show post category associaton on post grid

--- page/blogpost.php
+++ page/blogpost.php
@@ -16,6 +16,17 @@ class page_blogpost extends \xepan\base\Page {
 		$crud->setModel($blog_model)->setOrder('created_at','desc');
 		$crud->grid->addQuickSearch(['name']);
 		$crud->grid->addPaginator(50);
+
+		$crud->grid->addColumn('category');
+		$crud->grid->addMethod('format_postcategory',function($grid,$field){				
+				$data = $grid->add('xepan\blog\Model_Association_PostCategory')->addCondition('blog_post_id',$grid->model->id);
+				$l = $grid->add('Lister',null,'category',['view/post/post','category_lister']);
+				$l->setModel($data);
+				
+				$grid->current_row_html[$field] = $l->getHtml();
+		});
+
+		$crud->grid->addFormatter('category','postcategory');
 	}
 }
 
