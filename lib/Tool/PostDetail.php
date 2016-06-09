6df712e288e67627eb908b116ee691f69e929a89
dkanojia <dkanojia93@gmail.com>
2016-06-09 13:55:51 +0530

    reindention of file

--- lib/Tool/PostDetail.php
+++ lib/Tool/PostDetail.php
@@ -4,17 +4,16 @@ namespace xepan\blog;
 
 class Tool_PostDetail extends \xepan\cms\View_Tool{
 	public $options = [
-						'show_tag'=>true,
-						'show_image'=>true,
-						'show_comment_list'=>true,
-						'allow_anonymous_comment'=>true
+	'show_tag'=>true,
+	'show_image'=>true,
+	'show_comment_list'=>true,
+	'allow_anonymous_comment'=>true
 
-						];
+	];
 	public $post;
+
 	function init(){
 		parent::init();
-
-
 
 		$post_id = $this->api->stickyGET('post_id');
 
@@ -34,52 +33,52 @@ class Tool_PostDetail extends \xepan\cms\View_Tool{
 
 		if($sub_form->isSubmitted()){
 			
-      		if($this->options['allow_anonymous_comment'] == false){
-                $contact = $this->add('xepan\base\Model_Contact');
-      			if($contact->loadLoggedIn()){
+			if($this->options['allow_anonymous_comment'] == false){
+				$contact = $this->add('xepan\base\Model_Contact');
+				if($contact->loadLoggedIn()){
 
-      				$comment_model = $this->add('xepan\blog\Model_Comment');
+					$comment_model = $this->add('xepan\blog\Model_Comment');
 					$comment_model['comment'] = $sub_form['Comment'];
 					$comment_model['blog_post_id'] = $post_id;	
 					$comment_model['created_by_id'] = $contact->id;
 					$comment_model->save();
 
-		      		$sub_form->js(null,$sub_form->js()->reload())->univ()->successMessage('You have successfully commented on this post')->execute();
-      			}else{
-      				$this->api->memorize('next_url',array('page'=>$_GET['page'],'post_id'=>$_GET['post_id']));
-                	$this->app->redirect('login');
-      			}
-          	}
+					$sub_form->js(null,$sub_form->js()->reload())->univ()->successMessage('You have successfully commented on this post')->execute();
+				}else{
+					$this->api->memorize('next_url',array('page'=>$_GET['page'],'post_id'=>$_GET['post_id']));
+					$this->app->redirect('login');
+				}
+			}
 			
 			$comment_model = $this->add('xepan\blog\Model_Comment');
 			$comment_model['comment'] = $sub_form['Comment'];
 			$comment_model['blog_post_id'] = $post_id;	
 			
 			$contact = $this->add('xepan\base\Model_Contact');
-      		if($contact->loadLoggedIn()){
+			if($contact->loadLoggedIn()){
 				$comment_model['created_by_id'] = $contact->id;
 			}
 			
 			$comment_model->save();
-      		$sub_form->js(null,$sub_form->js()->reload())->univ()->successMessage('You have successfully commented on this post')->execute();
-      	}
+			$sub_form->js(null,$sub_form->js()->reload())->univ()->successMessage('You have successfully commented on this post')->execute();
+		}
 	}
 
 	function setModel($model){
 		$this->template->trySetHtml('comment_count', $model['comment_count']);
 		$this->template->trySetHtml('post_description', $model['description']);
-    
-		//comments
+		
+	//comments
 		$comnt_mdl = $this->add('xepan\blog\Model_Comment');
 		$cl = $this->add('CompleteLister',null,'comment_list',['view/tool/post/extra-detail/comment-list']);
 		$cl->setModel($comnt_mdl)->addCondition('blog_post_id',$this->post->id)->addCondition('status','Approved');
 		
 		parent::setModel($model);
 	}
+
 	function defaultTemplate(){
 		return ['view/tool/post/detail'];
 	}
-
 	
 	function addToolCondition_row_show_tag($value, $l){
 		
