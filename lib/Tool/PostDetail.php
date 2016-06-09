7bb6dbe7ea163550e898018f2410d7ad388fb640
dkanojia <dkanojia93@gmail.com>
2016-06-09 13:51:01 +0530

    on postdetail tool anonymous false condition rechecked & recitfication

--- lib/Tool/PostDetail.php
+++ lib/Tool/PostDetail.php
@@ -34,16 +34,21 @@ class Tool_PostDetail extends \xepan\cms\View_Tool{
 
 		if($sub_form->isSubmitted()){
 			
-      		if( $this->options['allow_anonymous_comment'] == "false"){
+      		if($this->options['allow_anonymous_comment'] == false){
                 $contact = $this->add('xepan\base\Model_Contact');
       			if($contact->loadLoggedIn()){
-					$this->app->redirect($this->api->url('blog-item'));
 
+      				$comment_model = $this->add('xepan\blog\Model_Comment');
+					$comment_model['comment'] = $sub_form['Comment'];
+					$comment_model['blog_post_id'] = $post_id;	
+					$comment_model['created_by_id'] = $contact->id;
+					$comment_model->save();
+
+		      		$sub_form->js(null,$sub_form->js()->reload())->univ()->successMessage('You have successfully commented on this post')->execute();
       			}else{
       				$this->api->memorize('next_url',array('page'=>$_GET['page'],'post_id'=>$_GET['post_id']));
-                	$this->app->redirect($this->api->url('login'));
+                	$this->app->redirect('login');
       			}
-                
           	}
 			
 			$comment_model = $this->add('xepan\blog\Model_Comment');
