dfd28364f43a4b4610c6610cda44da725973a168
vaibhav <vaibhavsharma38@gmail.com>
2016-06-29 11:19:32 +0530

    modifications made in comment model

--- page/comment.php
+++ page/comment.php
@@ -11,7 +11,14 @@ class page_comment extends \xepan\base\Page {
 		$blog_id = $this->app->stickyGET('blog_id');
 
 		$comment = $this->add('xepan\blog\Model_Comment');
+		$comment->addCondition('blog_post_id',$blog_id);			
+		
 		$crud = $this->add('xepan\hr\CRUD',null,'comments',['view\post\comment']);
+		
+		if($crud->isEditing('add')){
+			$comment->addCondition('created_by_id',$this->app->employee->id);			
+		}
+
 		$crud->setModel($comment)->setOrder('comment_date','desc');
 		$crud->grid->addQuickSearch(['comment']);
 		$crud->grid->addPaginator(50);
@@ -19,7 +26,9 @@ class page_comment extends \xepan\base\Page {
 		$blog = $this->add('xepan\blog\Model_BlogPost')
 			         ->load($blog_id);
 		$this->template->trySet('blog_title',$blog['title']);
+		
 	}
+
 
 	function defaultTemplate(){
 		return['view/post/commentblog'];
