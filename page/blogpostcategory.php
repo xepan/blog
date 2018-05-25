1c166eb02c2d5fa738536fbc60291f3d990d163e
monikapokra <monikagupta495@gmail.com>
2018-05-25 10:54:32 +0530

    form beautification

--- page/blogpostcategory.php
+++ page/blogpostcategory.php
@@ -12,9 +12,24 @@ class page_blogpostcategory extends \xepan\base\Page {
 		$blog_category_model->add('xepan\blog\Controller_SideBarStatusFilter');
 
 		$crud = $this->add('xepan\hr\CRUD',null,null,['view/post/category']);
-		$crud->setModel($blog_category_model);
+		if($crud->form){
+			$form = $crud->form;
+			$form->add('xepan\base\Controller_FLC')
+				->showLables(true)
+				->addContentSpot()
+				->layout([
+						'name'=>'Blog Post Category~c1~6',
+						'order'=>'c2~2',
+						'status'=>'c3~2',
+						'FormButtons~&nbsp;'=>'c4~2', // closed to make panel default collapsed
+						
+					]);
+		}
+		$crud->setModel($blog_category_model,['name','order','status']);
 		$crud->grid->addQuickSearch(['name']);
 		$crud->grid->addPaginator(10);
+
+
 	}
 }
 
